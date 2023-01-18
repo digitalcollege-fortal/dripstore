1bbe45ff2f4395dbac43ac880f8222585fc2fbe6
Victor <victoradler10@gmail.com>
2023-01-18 19:57:09 -0300

    create component icons highlight and paste images in repositories

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,6 +1,7 @@
 import Menu from "../../components/Menu";
 import Header from "../../components/Header";
 import Banner from "../../components/Banner";
+import HighlightIcon from "../../components/HighlightIcon";
 
 export default function Products() {
     return (
@@ -8,6 +9,7 @@ export default function Products() {
             <Header/>
             <Menu active="products"/>
             <Banner/>
+            <HighlightIcon/>
 
             Pagina de Produtos
         </div>
