b7255a1175d1e4889d9a2df350a143d411eb6dc3
Michael-Meneses <michaelfullstack@gmail.com>
2023-02-06 15:49:16 -0300

    feito toda parte de produtos

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,13 +1,16 @@
 import Menu from "../../components/Menu";
 import Header from "../../components/Header";
 import Banner from "../../components/Banner";
+import ResultSearch from "../../components/ResultSearch";
+import Footer from "../../components/Footer";
 
 export default function Products() {
     return (
         <div>
             <Header/>
             <Menu active="products"/>
-            <Banner/>
+            <ResultSearch/>
+            <Footer/>
 
             Pagina de Produtos
         </div>
